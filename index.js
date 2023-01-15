c8384bcc9706cd557f44699b211897b21b50e641
revdancatt <github@revdancatt.com>
2023-01-15 10:49:08 +0000

    Get the paper onto the canvas

---
+++ index.js
@@ -0,0 +1,188 @@
+/* global preloadImagesTmr fxhash fxrand paper1Loaded */
+
+//
+//  fxhash - fffffrfffffl
+//
+//
+//  HELLO!! Code is copyright revdancatt (that's me), so no sneaky using it for your
+//  NFT projects.
+//  But please feel free to unpick it, and ask me questions. A quick note, this is written
+//  as an artist, which is a slightly different (and more storytelling way) of writing
+//  code, than if this was an engineering project. I've tried to keep it somewhat readable
+//  rather than doing clever shortcuts, that are cool, but harder for people to understand.
+//
+//  You can find me at...
+//  https://twitter.com/revdancatt
+//  https://instagram.com/revdancatt
+//  https://youtube.com/revdancatt
+//
+
+const ratio = 1.414 // 1.414 is the golden ratio
+
+// const startTime = new Date().getTime() // so we can figure out how long since the scene started
+let drawn = false
+let highRes = false // display high or low res
+let drawPaper = true
+const features = {}
+let resizeTmr = null
+
+window.$fxhashFeatures = {}
+
+//  Work out what all our features are
+const makeFeatures = () => {
+  features.paperOffset = {
+    paper1: {
+      x: fxrand(),
+      y: fxrand()
+    },
+    paper2: {
+      x: fxrand(),
+      y: fxrand()
+    }
+  }
+}
+
+//  Call the above make features, so we'll have the window.$fxhashFeatures available
+//  for fxhash
+makeFeatures()
+
+console.log(features)
+console.table(window.$fxhashFeatures)
+
+const init = async () => {
+  //  I should add a timer to this, but really how often to people who aren't
+  //  the developer resize stuff all the time. Stick it in a digital frame and
+  //  have done with it!
+  window.addEventListener('resize', async () => {
+    //  If we do resize though, work out the new size...
+    clearTimeout(resizeTmr)
+    resizeTmr = setTimeout(async () => {
+      await layoutCanvas()
+    }, 100)
+  })
+
+  //  Now layout the canvas
+  await layoutCanvas()
+}
+
+const layoutCanvas = async () => {
+  const wWidth = window.innerWidth
+  const wHeight = window.innerHeight
+  let cWidth = wWidth
+  let cHeight = cWidth * ratio
+  if (cHeight > wHeight) {
+    cHeight = wHeight
+    cWidth = wHeight / ratio
+  }
+  const canvas = document.getElementById('target')
+  if (highRes) {
+    canvas.height = 8192
+    canvas.width = 8192 / ratio
+  } else {
+    canvas.width = Math.min((8192 / 2), cWidth * 2)
+    canvas.height = Math.min((8192 / ratio / 2), cHeight * 2)
+    //  Minimum size to be half of the high rez cersion
+    if (Math.min(canvas.width, canvas.height) < 8192 / 2) {
+      if (canvas.width < canvas.height) {
+        canvas.height = 8192 / 2
+        canvas.width = 8192 / 2 / ratio
+      } else {
+        canvas.width = 8192 / 2
+        canvas.height = 8192 / 2 / ratio
+      }
+    }
+  }
+
+  canvas.style.position = 'absolute'
+  canvas.style.width = `${cWidth}px`
+  canvas.style.height = `${cHeight}px`
+  canvas.style.left = `${(wWidth - cWidth) / 2}px`
+  canvas.style.top = `${(wHeight - cHeight) / 2}px`
+
+  //  Re-Create the paper pattern
+  const paper1 = document.createElement('canvas')
+  paper1.width = canvas.width / 2
+  paper1.height = canvas.height / 2
+  const paper1Ctx = paper1.getContext('2d')
+  await paper1Ctx.drawImage(paper1Loaded, 0, 0, 1920, 1920, 0, 0, paper1.width, paper1.height)
+  features.paper1Pattern = paper1Ctx.createPattern(paper1, 'repeat')
+
+  const paper2 = document.createElement('canvas')
+  paper2.width = canvas.width / (22 / 7)
+  paper2.height = canvas.height / (22 / 7)
+  const paper2Ctx = paper2.getContext('2d')
+  await paper2Ctx.drawImage(paper1Loaded, 0, 0, 1920, 1920, 0, 0, paper2.width, paper2.height)
+  features.paper2Pattern = paper2Ctx.createPattern(paper2, 'repeat')
+
+  //  And draw it!!
+  drawCanvas()
+}
+
+const drawCanvas = async () => {
+  //  Let the preloader know that we've hit this function at least once
+  drawn = true
+
+  const canvas = document.getElementById('target')
+  const ctx = canvas.getContext('2d')
+  const w = canvas.width
+  const h = canvas.height
+
+  //  Lay down the paper texture
+  if (drawPaper) {
+    ctx.fillStyle = features.paper1Pattern
+    ctx.save()
+    ctx.translate(-w * features.paperOffset.paper1.x, -h * features.paperOffset.paper1.y)
+    ctx.fillRect(0, 0, w * 2, h * 2)
+    ctx.restore()
+  } else {
+    ctx.fillStyle = '#F9F9F9'
+    ctx.fillRect(0, 0, w, h)
+  }
+
+  // Call the draw function again
+  // aniFrame = window.requestAnimationFrame(drawCanvas)
+}
+
+const autoDownloadCanvas = async (showHash = false) => {
+  const element = document.createElement('a')
+  element.setAttribute('download', `fffffrfffffl_${fxhash}`)
+  element.style.display = 'none'
+  document.body.appendChild(element)
+  let imageBlob = null
+  imageBlob = await new Promise(resolve => document.getElementById('target').toBlob(resolve, 'image/png'))
+  element.setAttribute('href', window.URL.createObjectURL(imageBlob, {
+    type: 'image/png'
+  }))
+  element.click()
+  document.body.removeChild(element)
+}
+
+//  KEY PRESSED OF DOOM
+document.addEventListener('keypress', async (e) => {
+  e = e || window.event
+  // Save
+  if (e.key === 's') autoDownloadCanvas()
+
+  //   Toggle highres mode
+  if (e.key === 'h') {
+    highRes = !highRes
+    console.log('Highres mode is now', highRes)
+    await layoutCanvas()
+  }
+
+  // Toggle the paper texture
+  if (e.key === 't') {
+    drawPaper = !drawPaper
+    await layoutCanvas()
+  }
+})
+
+//  This preloads the images so we can get access to them
+// eslint-disable-next-line no-unused-vars
+const preloadImages = () => {
+  //  If paper1 has loaded and we haven't draw anything yet, then kick it all off
+  if (paper1Loaded !== null && !drawn) {
+    clearInterval(preloadImagesTmr)
+    init()
+  }
+}
